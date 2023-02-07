894e9ae52209077b8067bdd191487e59b4bfc064
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-07 17:43:16 +0900

    글 작성 페이지

--- src/components/common/wrtie/index.tsx
+++ src/components/common/wrtie/index.tsx
@@ -1,9 +1,85 @@
+import { useState, useRef,useEffect } from 'react';
 import * as S from './style';
-
+import upload from '../.././../assets/upload/upload.svg';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 export default function Write(){
+
+    const [title,SetTitle] = useState<string>('');
+    const [post,SetPost] = useState<string>('');
+
+    const onChange = (e:any) =>{
+        if(e.target.name === 'title') SetTitle(e.target.value);
+        else if(e.target.name === 'post') SetPost(e.target.value);
+    };
+
+    //----
+    const [imgList,setImgList] = useState<any>([])
+    //----
+
+    const imgRef = useRef<any>();
+    // 이미지 업로드 input의 onChange
+    const saveImgFile = (e:React.ChangeEvent<HTMLInputElement>):void => {
+        
+	    console.log(imgRef.current.files)
+        setImgList((prev:any) => [...prev , ...imgRef.current.files])
+         // 파일 객체
+
+        const form = new FormData();
+        imgList.forEach((e:string,idx:number) => {
+            form.append(idx.toString(),e)
+        });
+
+    };
+
     return(
         <S.WriteContainer>
-            <S.WriteInput placeholder='제목을 입력하세요...'/>
+            <style>{"body{background-color:#F9F9F9}"}</style>
+            <S.WriteForm>
+                <S.WriteText1>당신의 이야기를 들려주세요.</S.WriteText1>
+                <div>
+                    <S.WriteTitle placeholder='제목을 입력하세요...' onChange={onChange} name='title' value={title}/>
+                    <S.WriteImgShow>
+                        {
+                            imgList.length!==0
+                            ?
+                            <Splide
+                                options={{
+                                type: "loop",
+                                autoplay: true,
+                                arrows: false,
+                                pagination: false,
+                                interval: 2000,
+                                speed: 1000,
+                            }}
+                            >
+                                {imgList.map((img:any,idx:any) => {
+                                    return (
+                                        <SplideSlide key={idx}>
+                                            <>
+                                                <S.WrtieImgShow src={URL.createObjectURL(img)} />
+                                            </>
+                                        </SplideSlide>
+                                    );
+                                })}
+                            </Splide>
+                            :
+                            <S.WriteText2>이미지 미리보기</S.WriteText2>
+                        }
+                    </S.WriteImgShow>
+                </div>
+                <div>
+                    <S.WriteInput placeholder='글 작성...' value={post} name='post' onChange={onChange}/>
+                </div>
+                    <S.WriteUploadContainer>
+                        <img src={upload} alt='upload'/>
+                        <S.WriteUploadLabel htmlFor="Img">이미지 업로드하기</S.WriteUploadLabel>
+                        <S.WriteUploadFile type='file' id="Img" accept="image/*" multiple onChange={saveImgFile} ref={imgRef}/>
+                    </S.WriteUploadContainer>
+                <div>
+                    <S.WriteSubmitBtn type='submit'>작성하기</S.WriteSubmitBtn>
+                </div>
+            </S.WriteForm>
         </S.WriteContainer>
     );
 }
--- src/components/common/wrtie/style.ts
+++ src/components/common/wrtie/style.ts
@@ -1,15 +1,107 @@
 import styled from "styled-components";
 
 export const WriteContainer = styled.div`
-    text-align:center;
+    width: 100%;
+    height: calc(100vh - 66px);
 `;
 
-export const WriteInput = styled.input`
+export const WriteForm = styled.div`
     width: 1222px;
-    height: 139px;
-    margin-top:96px;
-    border:none;
-    outline:none;
-    background-color:#FFFFFF;
+    /* background-color:green; */
+    height:800px;
+    margin:0 auto; 
+    margin-top:66px;
 `;
-// F9F9F9
+
+export const WriteText1 = styled.h1`
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 38px;
+    padding-top:50px;
+`;
+
+export const WriteTitle = styled.input`
+    width: 822px;
+    height:90px;
+    margin-top:60px;
+    font-size:30px;
+    padding-left:37px;
+    outline:none;
+    border:none;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.05);
+    border-radius: 15px;
+`;
+
+export const WriteImgShow = styled.div`
+    width:  363px;
+    height: 363px;
+    background-color:#FFFFFF;
+    float: right;
+    box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.05);
+    border-radius: 15px;
+    margin-top:60px;
+    text-align : center;
+    vertical-align:middle;
+    color:#949191;
+`;
+
+export const WriteText2 = styled.div`
+    position: relative; 
+    top: 50%; 
+    left: 50%; 
+    transform: translate(-50%, -50%); 
+`;
+
+export const WrtieImgShow = styled.img`
+    width:  363px;
+    height: 363px;
+    border-radius: 15px;
+`;
+
+export const WriteInput = styled.textarea`
+    width: 822px;
+    height: 420px;
+    background-color:#FFFFFF;
+    margin-top: 20px;
+    outline: none;
+    border: none;
+    box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.05);
+    border-radius: 15px;
+    padding-top:45px;
+    padding-left:37px;
+    font-size:22px;
+    resize: none;
+`;
+
+export const WriteUploadContainer = styled.form`
+    float:right;
+    position: relative;
+    right: 210px;
+    bottom: 145px;
+    display:flex;
+    gap:5px;
+`;
+
+export const WriteUploadLabel = styled.label`
+    margin-top:8px;
+`;
+
+export const WriteUploadFile = styled.input`
+    display:none;
+`;
+
+export const WriteSubmitBtn = styled.button`
+    width:318px;
+    height:50px;
+    float:right;
+    position:relative;
+    left:130px;
+    bottom: 50px;
+    border-radius: 15px;
+    background-color:#FF7777;
+    outline:none;
+    border:none;
+    box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.05);
+    cursor: pointer;
+`;
