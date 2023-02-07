159b388ad21b9d5cfa4efff6caad599b52c3111b
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-02-07 12:21:51 +0900

    충돌 해결

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -5,28 +5,28 @@ import { ACCESS_KEY } from "../../constants/token/token.constant";
 import authRepository from "../../repository/auth/auth.repository";
 
 const useLogin = () => {
-  // const { search } = useLocation();
-  // const query = queryString.parse(search);
-  // const navigate = useNavigate();
-  // const request = useCallback(async () => {
-  //   // try {
-  //   //   // const { accessToken } = await authRepository.login({
-  //   //     code: query.code as string,
-  //   //   });
-  //     localStorage.setItem(ACCESS_KEY, accessToken);
-  //     navigate("/");
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, []);
-  // useEffect(() => {
-  //   if (query) {
-  //     request();
-  //   }
-  // }, [query]);
-  // return {
-  //   request,
-  // };
+  const { search } = useLocation();
+  const query = queryString.parse(search);
+  const navigate = useNavigate();
+  const request = useCallback(async () => {
+    // try {
+    //   // const { accessToken } = await authRepository.login({
+    //     code: query.code as string,
+    //   });
+      localStorage.setItem(ACCESS_KEY, accessToken);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  useEffect(() => {
+    if (query) {
+      request();
+    }
+  }, [query]);
+  return {
+    request,
+  };
 };
 
 export default useLogin;
