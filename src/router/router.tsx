da531c232240cf7b038138d16c0f3d5b11a123b9
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-02-07 09:33:45 +0900

    setting

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "../components/common/header";
 
 function Router() {
   return (
     <>
-      <Routes></Routes>
+      <Routes> </Routes>
     </>
   );
 }
