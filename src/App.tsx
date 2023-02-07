cc88d7ec89847a508d339fa01c8b9b59e6b17b99
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-02-07 09:37:37 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import PageTemplate from "./components/common/pageTemplate";
+import Router from "./router/router";
 
 function App() {
-  return <div className="App"></div>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <PageTemplate>
+            <Router />
+          </PageTemplate>
+        </BrowserRouter>
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
