743ec7435ce08e927ae83279580483379bc5b030
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-02-07 17:01:13 +0900

    게시물 api

---
+++ src/repository/post/post.param.ts
@@ -0,0 +1,8 @@
+export interface PostParam {
+  content: string;
+  title: string;
+}
+
+export interface deletePostByIdParam {
+  id: string;
+}
---
+++ src/repository/post/post.repository.ts
@@ -0,0 +1,14 @@
+import { customAxios } from "../../lib/axios/customAxios";
+import { deletePostByIdParam, PostParam } from "./post.param";
+
+class PostRepository {
+  public async createPost({ content, title }: PostParam): Promise<void> {
+    await customAxios.post("/post/create", { content, title });
+  }
+
+  public async deletePostById({ id }: deletePostByIdParam): Promise<void> {
+    await customAxios.post(`/post/delete/${id}`);
+  }
+}
+
+export default new PostRepository();
