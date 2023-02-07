e7511116e89d7bc3850a28d8331b82a98a000002
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-08 01:46:39 +0900

    write 태그 해결

--- src/components/common/wrtie/index.tsx
+++ src/components/common/wrtie/index.tsx
@@ -20,18 +20,13 @@ const Write = () => {
 
   const imgRef = useRef<any>();
   // 이미지 업로드 input의 onChange
+  const formData = new FormData();
   const saveImgFile = (e: React.ChangeEvent<HTMLInputElement>): void => {
     //console.log(imgRef.current.files);
     setImgList((prev: any) => [...prev, ...imgRef.current.files]);
     // 파일 객체
     // 이미지 업로드 input의 onChange
-    const formData = new FormData();
-    const saveImgFile = (e: React.ChangeEvent<HTMLInputElement>): void => {
-      // console.log(imgRef.current.files)
-      setImgList((prev: string) => [...prev, ...imgRef.current.files]);
-      // 파일 객체
-      console.log(formData);
-    };
+  };
     const onSubmit = async (e: any) => {
       if (title !== "" && contents !== "") {
         try {
@@ -61,8 +56,74 @@ const Write = () => {
       } else {
         window.alert("제대로 입력해주세요!");
       }
-    };
   };
+  return(
+      <S.WriteContainer>
+        <style>{"body{background-color:#F9F9F9}"}</style>
+        <S.WriteForm>
+          <S.WriteText1>당신의 이야기를 들려주세요.</S.WriteText1>
+          <div>
+            <S.WriteTitle
+              placeholder="제목을 입력하세요..."
+              onChange={onChange}
+              name="title"
+              value={title}
+            />
+            <S.WriteImgShow>
+              {imgList.length !== 0 ? (
+                <Splide
+                  options={{
+                    type: "loop",
+                    autoplay: true,
+                    arrows: false,
+                    pagination: false,
+                    interval: 2000,
+                    speed: 1000,
+                  }}
+                >
+                  {imgList.map((img: any, idx: any) => {
+                    return (
+                      <SplideSlide key={idx}>
+                        <S.WrtieImgShow src={URL.createObjectURL(img)} />
+                      </SplideSlide>
+                    );
+                  })}
+                </Splide>
+              ) : (
+                <S.WriteText2>이미지 미리보기</S.WriteText2>
+              )}
+            </S.WriteImgShow>
+          </div>
+          <div>
+            <S.WriteInput
+              placeholder="글 작성..."
+              value={contents}
+              name="post"
+              onChange={onChange}
+            />
+          </div>
+          <S.WriteUploadContainer>
+            <img src={upload} alt="upload" />
+            <S.WriteUploadLabel htmlFor="Img">
+              이미지 업로드하기
+            </S.WriteUploadLabel>
+            <S.WriteUploadFile
+              type="file"
+              id="Img"
+              accept="image/*"
+              multiple
+              onChange={saveImgFile}
+              ref={imgRef}
+            />
+          </S.WriteUploadContainer>
+          <div>
+            <S.WriteSubmitBtn type="submit" onClick={onSubmit}>
+              작성하기
+            </S.WriteSubmitBtn>
+          </div>
+        </S.WriteForm>
+      </S.WriteContainer>
+    );
  };
 
 
